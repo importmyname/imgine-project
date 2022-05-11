dbc909db1e883410acd3902a9b44be0181549a7a
Smach <copsmach@gmail.com>
2022-05-11 21:57:16 +0700

    add logo

--- Weather-forecast/script.js
+++ Weather-forecast/script.js
@@ -41,7 +41,7 @@ let weather = {
 
  
 
-      document.querySelector(".city").innerText = "Weather in " + name;
+      document.querySelector(".city").innerText = name;
       document.querySelector(".icon").src =
         "https://openweathermap.org/img/wn/" + icon + ".png";
       document.querySelector(".description").innerText = description;
